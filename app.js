6872607b557322546d69f195ca6e81e1f2059693
JazimMehmood <jazimmehmood219@gmail.com>
2020-06-14 16:38:55 +0500

    Now ready to commit

---
+++ app.js
@@ -0,0 +1,42 @@
+//  Chapter 1
+// Ex 1
+alert("Error! Please enter a valid password.");
+// Ex3
+alert("Welcome to JS Land...\nHappy Coding");
+// Ex 4
+alert("Welcome to Js Land...");
+alert("Happy Coding!\nPrevent This page from creating additional dialogs.");
+// Ex 5
+// alert("hello...i can runJS through my web browser's console")
+// Write the above statement in console of browser
+
+// Chapter 2
+// 1
+var username;
+// 2
+var myName = "Jazim Mehmood";
+// 3
+var message = "Hello World"
+alert(message);
+// 4
+var name = "Jhone Doe";
+var age = "15 years old";
+var quali = "Certified Mobile Application Development";
+alert(name);
+alert(age);
+alert(quali);
+// 5
+var piz;
+piz ="PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(piz);
+// 6
+var email = "jazimqureshi@outlook.com";
+alert("My email address is " + email) 
+// 7
+var book =" A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book)
+// 8
+document.write("Yah! I can write HTML content through JavaScript")
+// 9
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
